c60e769764321b6c34617da23bffe87dcd57dae4
myuwi <myuwi@proton.me>
2024-03-14 18:08:28 +0200

    refactor: make ws combinator generic

--- src/parser/combinator.rs
+++ src/parser/combinator.rs
@@ -1,9 +1,11 @@
 use nom::{character::complete::multispace0, sequence::delimited};
 
-pub fn ws<'a, O, E, F>(f: F) -> impl nom::Parser<&'a str, O, E>
+pub fn ws<I, O, E, F>(f: F) -> impl nom::Parser<I, O, E>
 where
-    E: nom::error::ParseError<&'a str>,
-    F: nom::Parser<&'a str, O, E>,
+    I: nom::InputTakeAtPosition,
+    <I as nom::InputTakeAtPosition>::Item: nom::AsChar + Clone,
+    E: nom::error::ParseError<I>,
+    F: nom::Parser<I, O, E>,
 {
     delimited(multispace0, f, multispace0)
 }
